2bc1b4c9de5036d83c5beb64f0947afa2dc3609a
Cody Sullivan <76827473+neuroCody@users.noreply.github.com>
2021-08-04 16:28:32 -0600

    Using Types

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -14,21 +14,50 @@ import { Component }  from "@angular/core"
 
 export class UserComponent {
     //properties
-    firstName = "John";
-    lastName = "Smith";
-    age = 30;
-    address= {
-        street: "50 Main St.",
-        city: "Boston",
-        state: "MA"
-    };
+    firstName: string
+    lastName: string
+    age: number
+    address: any
+    
+
+    foo: any
+    hasKid: boolean
+    numberArray: number[]
+    stringArray: string[]
+    mixedArray: any[]
+    myTuple: [string, number, boolean]
+    unusable: void
+    u: undefined
+    n: null
 
     //Methods
     constructor() {
-       
+        this.firstName = 'John'
+        this.lastName = 'Doe'
+        this.age = 30
+
+        this.address = {
+            street: '50 Mian St',
+            city: 'Boston',
+            state: 'MA',
+        }
+
+        this.hasKid = true
+        this.numberArray = [1,2,3]
+        this.stringArray = ['this', 'is', 'an', 'array', 'of', 'strings']
+        this.mixedArray = [true, undefined, 1, 'hello']
+        this.myTuple = ['hello', 2, true]
+        this.unusable = undefined
+        this.u = undefined
+        this.n = null
+        console.log(this.addNumbers(2, 3))
     }
 
     showAge() {
         return this.age + 2
+    }
+
+    addNumbers(num1: number, num2: number): number {
+        return num1 + num2 
     }
 }
