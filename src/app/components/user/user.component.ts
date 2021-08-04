b621f9ca516d78383431a5ec77e18e1fdcf07bd1
Cody Sullivan <76827473+neuroCody@users.noreply.github.com>
2021-08-04 16:52:01 -0600

    Cleanup & Interfaces

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component }  from "@angular/core"
+import { User } from '../../models/User'
 
 @Component({
     selector: 'app-user',
@@ -14,50 +15,20 @@ import { Component }  from "@angular/core"
 
 export class UserComponent {
     //properties
-    firstName: string
-    lastName: string
-    age: number
-    address: any
-    
-
-    foo: any
-    hasKid: boolean
-    numberArray: number[]
-    stringArray: string[]
-    mixedArray: any[]
-    myTuple: [string, number, boolean]
-    unusable: void
-    u: undefined
-    n: null
+    user: User
 
     //Methods
     constructor() {
-        this.firstName = 'John'
-        this.lastName = 'Doe'
-        this.age = 30
-
-        this.address = {
-            street: '50 Mian St',
-            city: 'Boston',
-            state: 'MA',
+        this.user = {
+            firstName: 'John',
+            lastName: 'Doe',
+            age: 30,
+            address: {
+                street: '50 Main Street',
+                city: 'Boston',
+                state: 'MA',
+            },
         }
-
-        this.hasKid = true
-        this.numberArray = [1,2,3]
-        this.stringArray = ['this', 'is', 'an', 'array', 'of', 'strings']
-        this.mixedArray = [true, undefined, 1, 'hello']
-        this.myTuple = ['hello', 2, true]
-        this.unusable = undefined
-        this.u = undefined
-        this.n = null
-        console.log(this.addNumbers(2, 3))
-    }
-
-    showAge() {
-        return this.age + 2
-    }
-
-    addNumbers(num1: number, num2: number): number {
-        return num1 + num2 
     }
 }
+
